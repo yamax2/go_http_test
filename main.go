ea628ce31f18df18f101749cf556138a85549e36
Maxim Tretyakov <max@tretyakov-ma.ru>
2019-06-07 19:26:35 +0500

    f

--- main.go
+++ main.go
@@ -1,20 +1,52 @@
 package main
 
 import (
-    "net/http"
-    "log"
+	"time"
+	"io"
+	"log"
+	"net/http"
 )
 
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Content-Type", "text/plain")
-    w.Write([]byte("This is an example server.\n"))
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			if k != "Server" {
+			    dst.Add(k, v)
+			}
+		}
+	}
+}
+
+type proxy struct {
+}
+
+func (p *proxy) ServeHTTP(wr http.ResponseWriter, r *http.Request) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", "https://webdav.yandex.ru" + r.RequestURI, nil)
+	req.Header.Add("Authorization", "OAuth ...")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(wr, "Server Error", http.StatusInternalServerError)
+		log.Fatal("ServeHTTP:", err)
+	}
+
+	defer resp.Body.Close()
+	copyHeader(wr.Header(), resp.Header)
+	wr.WriteHeader(resp.StatusCode)
+	io.Copy(wr, resp.Body)
 }
 
 func main() {
-    http.HandleFunc("/", HelloServer)
+	handler := &proxy{}
+	log.Println("Starting server on :9000")
 
-    err := http.ListenAndServe(":9000", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	srv := &http.Server{
+	    Addr: ":9000",
+	    Handler: handler,
+	    ReadTimeout:  5 * time.Second,
+	    WriteTimeout: 5 * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
 }
